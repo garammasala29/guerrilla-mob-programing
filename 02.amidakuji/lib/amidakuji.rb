7cab7d1e6729caeec0f322b43533faed8bb01a7d
yocajii <yocajii@stone.sakura.ne.jp>
2023-02-17 17:17:59 +0900

    横棒が各列に1本は表示されるようにした

--- 02.amidakuji/lib/amidakuji.rb
+++ 02.amidakuji/lib/amidakuji.rb
@@ -13,14 +13,19 @@ class Amidakuji
     lines.join("\n")
   end
 
+  # 名前を再考する
   def vertical_lines(number)
-    Array.new(number * 2, '|   |   |   |   |')
+    blank_rows = Array.new((number * 2) - 1, ['   ','   ','   ','   '])
+    rows = blank_rows << %w[--- --- --- ---]
+    cols = rows.transpose.map { |col| col.shuffle }
+    cols.transpose.map { |row| "|#{row.join('|')}|" }
   end
 
   def hit_mark(number)
     line = Array.new(number - 1, '   ')
     line << '@   '
     [line.shuffle.join]
+    # 当たりが2列目の時にスペーサーがずれるのをどうにかする
   end
 end
 
